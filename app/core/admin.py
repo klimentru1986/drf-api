bd418b8bf0b3f20144c22caa9093f0af495eadc9
Kliment <kliment1986@inbox.ru>
2023-04-21 13:17:07 +0300

    admin config

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.utils.translation import gettext as _
+from django.utils.translation import gettext_lazy as _
 
 from core import models
 
@@ -16,12 +16,15 @@ class UserAdmin(BaseUserAdmin):
          "fields": ("is_active", "is_staff", "is_superuser")}),
         (_("Important Dates"), {"fields": ("last_login",)})
     )
+    readonly_fields = ["last_login"]
 
     add_fieldsets = (
         (None, {
             "classes": ("wide",),
-            "fields": ("email", "password1", "password2")
-        }),
+            "fields": ("email", "password1", "password2")}),
+        (_("Personal Info"), {"fields": ("name",)}),
+        (_("Permissions"), {
+         "fields": ("is_active", "is_staff", "is_superuser")}),
     )
 
 
--- app/core/models.py
+++ app/core/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, \
-    PermissionsMixin
+from django.contrib.auth.models import (BaseUserManager, AbstractBaseUser, PermissionsMixin)
 
 
 class UserManager(BaseUserManager):
